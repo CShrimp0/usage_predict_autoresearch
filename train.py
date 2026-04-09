8a62051f3f8f930b7f1804103888265a8cf88377
Li-fight-for-dream <1620021303@qq.com>
2026-04-09 15:58:38 +0800

    exp: use silu in the fusion gate

--- train.py
+++ train.py
@@ -319,7 +319,7 @@ class AgeRegressor(nn.Module):
         gate_hidden_dim = max(32, fused_dim // 16)
         self.fusion_gate = nn.Sequential(
             nn.Linear(fused_dim, gate_hidden_dim),
-            nn.ReLU(inplace=True),
+            nn.SiLU(),
             nn.Linear(gate_hidden_dim, fused_dim),
             nn.Sigmoid(),
         )
