610ddd612822c0ff7efe765444dfc534381dc539
Li-fight-for-dream <1620021303@qq.com>
2026-04-11 18:09:23 +0800

    Revert "research: remove aux concat from FiLM head"
    This reverts commit fa7d65e67befac94bf793f4df9767dfe23a0e029.

--- train.py
+++ train.py
@@ -313,7 +313,7 @@ class AgeRegressor(nn.Module):
                 nn.ReLU(inplace=True),
             )
             self.image_film = nn.Linear(cfg.aux_hidden_dim, image_feature_dim * 2)
-            fused_dim = image_feature_dim
+            fused_dim = image_feature_dim + cfg.aux_hidden_dim
         else:
             self.aux_branch = None
             self.image_film = None
@@ -335,7 +335,7 @@ class AgeRegressor(nn.Module):
             aux_repr = self.aux_branch(aux_features)
             gamma, beta = self.image_film(aux_repr).chunk(2, dim=1)
             image_features = image_features * (1.0 + 0.1 * torch.tanh(gamma)) + 0.1 * beta
-            fused = image_features
+            fused = torch.cat([image_features, aux_repr], dim=1)
         else:
             fused = image_features
         return self.head(fused).squeeze(-1)
