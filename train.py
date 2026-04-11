201c07dbbab54cfd9c3c7e7669a1d24dd24c8a01
Li-fight-for-dream <1620021303@qq.com>
2026-04-12 00:59:14 +0800

    Revert "exp: fused batchnorm"
    This reverts commit c0ac00e30b8bfb0c3a20ece89762e83ac167cfa6.

--- train.py
+++ train.py
@@ -320,7 +320,6 @@ class AgeRegressor(nn.Module):
             self.image_film = None
             fused_dim = image_feature_dim
 
-        self.fused_bn = nn.BatchNorm1d(fused_dim)
         self.head = nn.Sequential(
             nn.Linear(fused_dim, 256),
             nn.ReLU(inplace=True),
@@ -346,7 +345,6 @@ class AgeRegressor(nn.Module):
             fused = torch.cat([image_features, aux_repr], dim=1)
         else:
             fused = image_features
-        fused = self.fused_bn(fused)
         return self.head(fused).squeeze(-1)
 
 
