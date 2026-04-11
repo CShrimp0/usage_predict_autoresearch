3852c74321ca0cda77725d82c9e9285839c9d99a
Li-fight-for-dream <1620021303@qq.com>
2026-04-12 03:00:44 +0800

    exp: head SiLU

--- train.py
+++ train.py
@@ -323,10 +323,10 @@ class AgeRegressor(nn.Module):
 
         self.head = nn.Sequential(
             nn.Linear(fused_dim, 256),
-            nn.ReLU(inplace=True),
+            nn.SiLU(inplace=True),
             nn.Dropout(cfg.dropout),
             nn.Linear(256, 128),
-            nn.ReLU(inplace=True),
+            nn.SiLU(inplace=True),
             nn.Dropout(cfg.dropout * 0.5),
             nn.Linear(128, 1),
         )
