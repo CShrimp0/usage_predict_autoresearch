13797a2b3f48bee71b08d6dd0941762b44979a3e
Li-fight-for-dream <1620021303@qq.com>
2026-04-11 20:14:38 +0800

    Revert "exp: use rtm_huber loss"
    This reverts commit d8476a3a1ccf814f105337c7936f476d042d8258.

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ class ExperimentConfig:
     warmup_epochs: int = 5
     max_grad_norm: float = 1.0
 
-    loss: str = "rtm_huber"
+    loss: str = "mae"
     huber_delta: float = 1.0
     lambda_rtm: float = 0.1
     use_ema: bool = True
