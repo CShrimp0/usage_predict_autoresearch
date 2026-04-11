b1f6cf8466d85f40c72076e4e6e0fa796bac4c12
Li-fight-for-dream <1620021303@qq.com>
2026-04-11 17:57:09 +0800

    research: learnable FiLM modulation scales

--- train.py
+++ train.py
@@ -313,6 +313,8 @@ class AgeRegressor(nn.Module):
                 nn.ReLU(inplace=True),
             )
             self.image_film = nn.Linear(cfg.aux_hidden_dim, image_feature_dim * 2)
+            self.film_gamma_scale = nn.Parameter(torch.tensor(0.1))
+            self.film_beta_scale = nn.Parameter(torch.tensor(0.1))
             fused_dim = image_feature_dim + cfg.aux_hidden_dim
         else:
             self.aux_branch = None
@@ -334,7 +336,9 @@ class AgeRegressor(nn.Module):
         if self.aux_branch is not None and aux_features is not None:
             aux_repr = self.aux_branch(aux_features)
             gamma, beta = self.image_film(aux_repr).chunk(2, dim=1)
-            image_features = image_features * (1.0 + 0.1 * torch.tanh(gamma)) + 0.1 * beta
+            gamma_scale = self.film_gamma_scale.clamp(0.0, 0.5)
+            beta_scale = self.film_beta_scale.clamp(0.0, 0.5)
+            image_features = image_features * (1.0 + gamma_scale * torch.tanh(gamma)) + beta_scale * beta
             fused = torch.cat([image_features, aux_repr], dim=1)
         else:
             fused = image_features
