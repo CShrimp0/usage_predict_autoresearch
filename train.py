fffc287469ddf2e4ee1d3d49b82b6f9bb3fdf882
Li-fight-for-dream <1620021303@qq.com>
2026-04-12 02:32:54 +0800

    exp: batch size 24

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ class ExperimentConfig:
     saturation_jitter: float = 0.0
     hue_jitter: float = 0.0
 
-    batch_size: int = 16
+    batch_size: int = 24
     num_workers: int = 8
     epochs: int = 500
     patience: int = 100
